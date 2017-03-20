607221e7334c4cb5a7edea872180486035c574ab
Daniel Cardoza <daniel.cardoza@aerofs.com>
2017-03-20 02:50:03 -0400

    [datasets] Retrieving wav, beat files from songs 1MilSongDataset works

--- scripts/audio_converter.py
+++ scripts/audio_converter.py
@@ -1,6 +1,20 @@
 import subprocess
 
-# Requires ffmpeg to be in your $PATH environment variable
-
+# Converts between two audio file formats derived from the file extensions
+# Only tested from .mp3 -> .wav
 def convert_audio_file(input_file, output_file):
-    return subprocess.call('ffmpeg -i {} {}'.format(input_file, output_file))
+    converters = [
+        ['ffmpeg', '-i', input_file, output_file],
+        ['avconv', '-i', input_file, output_file],
+    ]
+
+    for c in converters:
+        try:
+            ret = subprocess.call(c)
+            return
+        except OSError as e:
+            continue
+    raise ConversionFailedException()
+
+class ConversionFailedException(Exception):
+    pass
--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -1,14 +1,21 @@
 from gmusicapi import Mobileclient
 from gmusicapi import Webclient
+from hdf5 import hdf5_getters
 import json
 import os
 import sys
 import urllib2
-import hdf5
+import audio_converter
 
-# This program 
+# This program transforms HDF5 files into .mp3, .beat files per song
+# Steps performed:
+#  - Extract song metadata from a song in the MillionSongSubset
+#  - Download song from GooglePlayMusic in mp3 form
+#  - Convert mp3 song to wav
+#  - Write beats file for song
 
-
+def usage():
+    print "usage: python generate_dataset.py <root_dir_of_hdf5_files> <output_directory> <#_of_songs_to_extract>"
 
 # A wrapper class allowing you to download songs from GooglePlayMusic
 class GMSongClient():
@@ -17,10 +24,11 @@ class GMSongClient():
     def __init__(self):
         email = os.environ['EMAIL']
         pw = os.environ['PASSWORD']
-        self.api = Mobileclient()
+        self.api = Mobileclient(debug_logging=False)
         self.api.login(email, pw, Mobileclient.FROM_MAC_ADDRESS, 'en_us')
 
-    def download_song(self, artist, title):
+    # Download a given song to a specified directory
+    def download_song(self, artist, title, output_dir):
         query = '{} - {}'.format(artist, title)
         print 'Looking for', query
 
@@ -39,8 +47,7 @@ class GMSongClient():
             except:
                 pass
         if song == '':
-            print 'Unable to find song'
-            return
+            raise SongNotFoundException
 
         # Extract mp3 url
         store_id = song['track']['storeId']
@@ -49,15 +56,17 @@ class GMSongClient():
 
         # Download mp3 file
         mp3file = urllib2.urlopen(url)
-        file_name = '{}.mp3'.format(query)
-        with open(query,'wb') as output:
+        file_name = '{}{}.mp3'.format(output_dir, query)
+        with open(file_name, 'wb') as output:
             output.write(mp3file.read())
 
-#c = GMSongClient()
-#c.download_song('Blink-182', 'What\'s My Age Again?')
+class SongNotFoundException(Exception):
+        pass
 
-def usage():
-    print "usage: python generate_dataset.py <root_dir_of_hdf5_files> <output_directory> <#_of_songs_to_extract>"
+def write_beats_file(file_name, beats):
+    text = ",".join(beats)
+    with open(file_name, "w") as f:
+        f.write(text)
 
 def main():
     count = 0
@@ -68,14 +77,42 @@ def main():
     data_dir = sys.argv[1]
     output_dir = sys.argv[2]
     num_songs = int(sys.argv[3])
+    c = GMSongClient()
 
+    # Walk the dataset tree
     for root, dirs, files in os.walk(data_dir):
         for f in files:
-            print f
+            file_name = "/".join([root,f])
+            print "Parsing file {}".format(file_name)
+            
+            # Retrieve HDF5 metadata
+            h5 = hdf5_getters.open_h5_file_read(file_name)
+            artist = hdf5_getters.get_artist_name(h5)
+            song_title = hdf5_getters.get_title(h5)
+            beats = ["%.4f" % beat for beat in hdf5_getters.get_beats_start(h5) ]
+            song_name = "{} - {}".format(artist, song_title)
+            print "Attempting to download song {}".format(song_name)
+
+            # Download song
+            try:
+                c.download_song(artist, song_title, output_dir)
+            except SongNotFoundException as e:
+                print "Unable to download {}".format(song_name)
+                continue
+
+            # Convert song from mp3 to .wav
+            base_filename = output_dir + song_name
+            audio_converter.convert_audio_file(base_filename + ".mp3", base_filename + ".wav")
+           
+            # Write beats file
+            write_beats_file(output_dir + song_name + ".beats", beats)
+
+            print "Successfully downloaded {}".format(song_name)
             count+=1
-            if count > 50:
+            if count >= num_songs:
                 break
-        if count > 50:
+
+        if count >= num_songs:
             break
 
 if __name__ == '__main__':
