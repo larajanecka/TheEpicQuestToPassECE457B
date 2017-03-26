d2e3fd69fa99e200d6a6fc041b79b5451afce405
Daniel Cardoza <daniel.cardoza@aerofs.com>
2017-03-26 19:34:20 -0400

    [audio_converter] Don't log subprocess output
    [step_predictor] Predict if a given beat is a step
    
    - use beat porbability and the time from the last step
    
    [dataset] Misc errors

---
+++ scripts/beat_predictor.py
@@ -0,0 +1,26 @@
+import random
+
+# Predict when the next 
+# period - the length of time for each array entry
+# Example : beats = [0.4, 0.0, 0.1, 0.7], period = 0.5 (seconds) for a song of 2s
+def predict_steps(beats, period):
+    # Return the times when steps occur
+    steps = []
+
+    # If more than 1s has passed, it is a step
+    # If less than 1s has passed, prob = diff_time
+    def probability_of_step(last_step, beat_time):
+        diff = beat_time - last_step
+        if diff > 1:
+            return 1.0
+        return diff
+
+    # Calculate the probability a given beat is a step given the probability
+    # the time is a beat and the time since the last step
+    for b in beats:
+        last_step_probability = probability_of_step(steps[-1] if len(steps) > 0 else 0, b)
+        probability = b * last_step_probability
+        if random.uniform(0.0, 1.0) <= probability:
+            steps.append(b * period)
+
+   return steps 
