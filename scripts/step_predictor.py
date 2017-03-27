abcd079cc64f00fffd3c30a9dd40b721fb55e3de
Daniel Cardoza <daniel.cardoza@aerofs.com>
2017-03-26 20:05:15 -0400

    [step predictor] Output format contains whether each time frame is a beat

--- scripts/step_predictor.py
+++ scripts/step_predictor.py
@@ -1,8 +1,8 @@
 import random
 
-# Predict when the next 
-# period - the length of time for each array entry
-# Example : beats = [0.4, 0.0, 0.1, 0.7], period = 0.5 (seconds) for a song of 2s
+# Predict when the next step will occur, given the probability a time_frame represents a beat
+# period - the length of time (s) for each timeframe
+# Example : beats = [0.4, 0.0, 0.1, 0.7], period = 0.5 for a song of 2s
 def predict_steps(beats, period):
     # Return the times when steps occur
     steps = []
@@ -10,16 +10,20 @@ def predict_steps(beats, period):
     # If more than 1s has passed, it is a step
     # If less than 1s has passed, prob = diff_time^3
     def probability_of_step(last_step, beat_time):
-        if diff > 1:
+        diff = beat_time - last_step
+        if diff >= 1:
             return 1.0
-        return (beat_time - last_step) ** 3
+        return (diff) ** 3
 
     # Calculate the probability a given beat is a step given the probability
     # the time is a beat and the time since the last step
     for b in beats:
-        last_step_probability = probability_of_step(steps[-1] if len(steps) > 0 else 0, b)
+        last_step = steps[-1] if len(steps) > 0 else 0 
+        last_step_probability = probability_of_step(last_step, b)
         probability = b * last_step_probability
         if random.uniform(0.0, 1.0) <= probability:
-            steps.append(b * period)
+            steps.append(1.0)
+        else
+            steps.append(0.0)
 
    return steps 
