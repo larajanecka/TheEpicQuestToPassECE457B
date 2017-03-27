59f6b419f23a3036e8e916b87eb3e202c8612271
Lara Janecka <numbersandlettersandallthat@gmail.com>
2017-03-26 22:46:54 -0400

    small fixes

--- scripts/featureExtractor.py
+++ scripts/featureExtractor.py
@@ -1,13 +1,27 @@
 import wavParser
 from math import floor
-from scipy.fftpack import fft
+from scipy.fftpack import fft, fftfreq
 from numpy import sqrt, arctan, pi
 import matplotlib.pyplot as plt
+# add calculation to get peak frequency
+# calculate change in peak frequency with next value
+# change aggregation functions to have overlap
 
 # Size of the chunk to aggregate
 chunkSize = 441
 # Frequency bands
-breakFrequencies = [2000,4000,8000,16000,32000]
+breakFrequencies = [200,400,800,1600,3200]
+
+
+# sum channels
+def sumChannels(waveform):
+	summed = []
+	for i in range(0, len(waveform[0])):
+		total = 0
+		for k in range(0, len(waveform)):
+			total += waveform[k][i]**2
+		summed.append(total)
+	return summed
 
 # iterate over total waveform and average chunks
 def chunkDataAverage(waveform):
@@ -17,9 +31,7 @@ def chunkDataAverage(waveform):
 		# sum energy level over chunk
 		total = 0
 		for j in range(i, i+chunkSize):
-			# calculate energy level, summed square over all channels
-			for k in range(0, len(waveform))
-				total += waveform[k][j]**2
+			total += waveform[j]
 		averagedAmplitudes.append(total / chunkSize)
 	return averagedAmplitudes
 
@@ -37,13 +49,11 @@ def getVariances(waveform, sampleRate):
 	variances = []
 	for i in range(0, numSamples):
 		windowSize = chunksPerSecond
-
 		if i >= halfWindowSize:
 			windowSize += i
 
 		if i < numSamples - halfWindowSize:
-			windowSize -= (numSamples - i)
-
+			windowSize -= (numSamples - i + 1)
 
 		variances.append(waveform[i] - (secondWindowSum / windowSize))
 
@@ -63,32 +73,42 @@ def testVarience():
 	variances = getVariances(averagedWaveform, sampleRate)
 	return variences
 
-# pass in a chunk's worth of data
-def chunkBandwidthAverage(waveform, sampleRate):
-	bandWidthAverages = []
-	# TODO: check this
-	hertzPerBin = sampleRate / chunkSize
 
-	for i in range(0, len(waveform), chunk):
-		chunkAverages = []
-		fourier = fft(waveform[i:i+chunk])
+def chunkBandwidthEnergies(waveform, sampleRate):
+	bandwidthEnergies = []
+
+	freqs = fftfreq(chunkSize, 0.000022676)
+	for i in range(0, len(waveform), chunkSize):
+		chunkEnergies = []
+		fourier = fft(waveform[i:i+chunkSize])
 		binNumber = 0
-		binCount = 0
 		binTotal = 0
-		for j in fourier:
-			if j*hertzPerBin > breakFrequencies[binNumber] :
-				chunkAverages.append(binTotal/binCount)
+		maxFreq = 0
+		maxAmp = 0
+		# data is symmetrical, ignore last half
+		for j in range(1, int(len(fourier)/2 + 1)):
+			# ignore frequencies above 3200
+			if freqs[j] > breakFrequencies[-1]:
+				break
+			value = fourier[j]
+			# sum energy in band width
+			if binNumber < len(breakFrequencies) and freqs[j] > breakFrequencies[binNumber] :
+				chunkEnergies.append(binTotal)
 				binNumber += 1
 				binTotal = 0
-				binCount = 0
-			amp = sqrt(i.real**2 + i.imag**2)
+			amp = value.real**2 + value.imag**2
 			binTotal += amp
-			binCount ++
-		bandwidthAverages.append(chunkAverages)
-	return bandwidthAverages
+
+			# keep track of frequency with max amplitude
+			maxAmp = max(maxAmp, amp)
+			if maxAmp == amp:
+				maxFreq = freqs[j]
+		bandwidthEnergies.append(chunkEnergies)
+		bandwidthEnergies.append(maxFreq)
+	return bandwidthEnergies
 
 # pass in a second's worth of data
-def getBandwidthVariance(dataSet):
+def getBandwidthVariance(dataSet, sampleRate):
 	numSamples = len(dataSet)
 	bands = len(breakFrequencies)
 	chunksPerSecond = int(floor(sampleRate/chunkSize))
@@ -109,7 +129,7 @@ def getBandwidthVariance(dataSet):
 			windowSize += i
 
 		if i < numSamples - halfWindowSize:
-			windowSize -= (numSamples - i)
+			windowSize -= (numSamples - i + 1)
 
 		bandVariance = []
 		for j in range(0, bands):
@@ -126,7 +146,19 @@ def getBandwidthVariance(dataSet):
 def testBandWidthVariences(waveform):
 	filename = "Ltheme2.wav"
 	waveform, sampleRate, bitsPerSample = wavParser.getRawWaveData(filename)
-	averages = chunkBandwidthAverage(waveform, sampleRate)
+	averages = chunkBandwidthEnergies(waveform, sampleRate)
 	variances = getBandwidthVariance(averages)
 
 
+def getFeatures(filename):
+	waveform, sampleRate, bitsPerSample = wavParser.getRawWaveData(filename)
+	summedWaveform = sumChannels(waveform)
+	# averagedWaveform = chunkDataAverage(summedWaveform)
+	# variances = getVariances(averagedWaveform, sampleRate)
+	bandwidthAverages = chunkBandwidthEnergies(summedWaveform, sampleRate)
+	print bandwidthAverages[0]
+	print summedWaveform[0]
+	# bandwidthAverages = getBandwidthVariance(bandwidthAverages, sampleRate)
+
+
+getFeatures("Ltheme2.wav")
