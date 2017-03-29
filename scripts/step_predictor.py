47cae956587bf4d2f503b59f644534db531460b4
Hong Wang <hmwang@uwaterloo.ca>
2017-03-29 15:43:09 -0400

    predictor change

--- scripts/step_predictor.py
+++ scripts/step_predictor.py
@@ -11,21 +11,24 @@ def predict_steps(beats, period):
 
     # If more than 1s has passed, it is a step
     # If less than 1s has passed, prob = diff_time^3
-    def probability_of_step(last_step, beat_time):
-        diff = beat_time - last_step
-        if diff >= 1:
-            return 1.0
-        return (diff) ** 3
+    def probability_of_step(last_step):
+        last_step = 1 if last_step > 1 else last_step
+        return last_step ** 2
 
     # Calculate the probability a given beat is a step given the probability
     # the time is a beat and the time since the last step
+    lastStepIndex = 0
+    i = 0
     for b in beats:
-        last_step_time = len(steps)*period if len(steps) > 0 else 0 
-        last_step_probability = probability_of_step(last_step_time, b)
+        last_step_time = (len(steps) - lastStepIndex)*period if len(steps) > 0 else 0 
+        last_step_probability = probability_of_step(last_step_time)
         probability = b * last_step_probability
+        #print "%s %s " %(last_step_time, last_step_probability)
         if random.uniform(0.0, 1.0) <= probability:
             steps.append(1.0)
-        else
+            lastStepIndex = i
+        else:
             steps.append(0.0)
+        i += 1
 
-   return steps 
+    return steps 
